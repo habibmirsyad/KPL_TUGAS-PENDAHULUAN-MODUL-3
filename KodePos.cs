f16c3e9e9130f76f92d58a51caa1ab045930921f
habibmirsyad <habmirs@gmail.com>
2022-03-10 17:07:43 +0700

    Inisialisasi Project

---
+++ KodePos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul3_1302201138
+{
+    public class KodePos
+    {
+        public string getKodePos(string kelurahan)
+        {
+            Console.Write("Kode Pos: ");
+            switch (kelurahan)
+            {
+                case "Batununggal":
+                    return "40266";
+                    break;
+                case "Kujangsari":
+                    return "40287";
+                    break;
+                case "Mengger":
+                    return "40267";
+                    break;
+                case "Wates":
+                    return "40256";
+                    break;
+                case "Cijaura":
+                    return "40287";
+                    break;
+                case "Jatisari":
+                    return "40286";
+                    break;
+                case "Margasari":
+                    return "40286";
+                    break;
+                case "Sekejati":
+                    return "40286";
+                    break;
+                case "Kebonwaru":
+                    return "40272";
+                    break;
+                case "Maleer":
+                    return "40274";
+                    break;
+                case "Samoja":
+                    return "40273";
+                    break;
+                default:
+                    return "Kelurahan tidak terdaftar.";
+            }
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul3_1302201138
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            //Inisialisasi
+            KodePos kp = new KodePos();
+
+            //Pemanggilan method
+            Console.WriteLine("note: huruf pertama harus kapital!\n");
+            Console.Write("Masukan nama kelurahan: ");
+            string kel = Console.ReadLine();
+            Console.WriteLine(kp.getKodePos(kel));
+        }
+    }
+}
