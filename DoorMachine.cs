7587dd4c86791c49efaf75083a333439666cd0c7
habibmirsyad <habmirs@gmail.com>
2022-03-11 17:07:00 +0700

    Menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul3_1302201138
+{
+    internal class DoorMachine
+    {
+        public string State(string pilihan)
+        {
+            switch (pilihan)
+            {
+                case "1":
+                    return "Pintu Terkunci";
+                    break;
+                case "2":
+                    return "Pintu Terbuka";
+                    break;
+                case "0":
+                    return "Keluar dari program.";
+                    break;
+                case default:
+                    return "Pilihan tidak tersedia.";
+            }
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -12,12 +12,26 @@ namespace tpModul3_1302201138
         {
             //Inisialisasi
             KodePos kp = new KodePos();
+            DoorMachine dm = new DoorMachine();
 
-            //Pemanggilan method
+            //Pemanggilan method kelurahan dan kode pos
             Console.WriteLine("note: huruf pertama harus kapital!\n");
             Console.Write("Masukan nama kelurahan: ");
             string kel = Console.ReadLine();
             Console.WriteLine(kp.getKodePos(kel));
+            
+            //Pemanggilan method Door Machine
+            string pilihan = "1";
+
+            while (pilihan != "0")
+            {
+                Console.WriteLine("1. Tutup Pintu");
+                Console.WriteLine("2. Buka Pintu");
+                Console.WriteLine("0. Keluar");
+                Console.Write("Masukkan pilihan anda: ");
+                pilihan = Console.ReadLine();
+                Console.WriteLine(dm.State(pilihan));
+            }
         }
     }
 }
